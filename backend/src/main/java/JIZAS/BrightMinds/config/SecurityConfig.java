c0445ea4e118660fb7ec78c2fe490a9005f9010b
Isaac <quiloisaac@gmail.com>
2025-09-24 00:59:21 +0800

    added vercel deployment url to security config

--- backend/src/main/java/JIZAS/BrightMinds/config/SecurityConfig.java
+++ backend/src/main/java/JIZAS/BrightMinds/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173" /* change to host on prod deployment */));
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:5173", "brightminds-cit.vercel.app" /* change to host on prod deployment */));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
